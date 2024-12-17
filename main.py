2a3edb8b0f3286e97e28be932af1ef906c680753
candog85 <candog851@gmail.com>
2024-12-17 10:50:56 -0500

    Did stuff?

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 import pymysql
 from dynaconf import Dynaconf
 
@@ -7,6 +7,9 @@ app = Flask(__name__)
 conf = Dynaconf(
     settings_file=['settings.toml']
 )
+
+app.secret_key = conf.secret_key
+
 
 def connect_db():
     conn = pymysql.connect(
@@ -20,38 +23,84 @@ def connect_db():
 
     return conn
 
+
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
 
+
 @app.route("/browse")
 def browse():
-    
-    conn=connect_db()
-    
-    cursor=conn.cursor()
-    
+
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
     cursor.execute("SELECT * FROM `Product`;")
-    
-    results=cursor.fetchall()
+
+    results = cursor.fetchall()
 
     cursor.close()
     conn.close()
-    
+
     return render_template("browse.html.jinja", products=results)
+
 
 @app.route("/product/<product_id>")
 def product_page(product_id):
 
-    conn=connect_db()
-    
-    cursor=conn.cursor()
+    conn = connect_db()
 
-    cursor.execute(f"SELECT * FROM `Product` WHERE `product_id` = {product_id};")
+    cursor = conn.cursor()
 
-    result=cursor.fetchone()
+    cursor.execute(
+        f"SELECT * FROM `Product` WHERE `product_id` = {product_id};")
+
+    result = cursor.fetchone()
 
     cursor.close()
     conn.close()
 
     return render_template("product.html.jinja", product=result)
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup_page():
+
+    if request.method == "POST":
+        full_name = request.form['full_name']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        phone = request.form['phone']
+        address = request.form['address']
+        confirm_password = request.form['confirm_password']
+
+        if password != confirm_password:
+            flash("Confirmed password does not match")
+            return render_template("signup.html.jinja")
+
+        conn = connect_db()
+
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(f"""
+
+                INSERT INTO `Customer`
+                    (`full_name`, `username`, `password`, `email`, `phone`, `address`)
+                VALUES
+                    ('{full_name}', '{username}', '{password}', '{email}', '{phone}', '{address}');
+            """)
+
+        except pymysql.err.IntegrityError:
+            flash("Your email/username already", 'error')
+
+        else:
+            return redirect("/signin")
+
+        finally:
+            cursor.close()
+            conn.close()
+
+    return render_template("signup.html.jinja")
