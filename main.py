075f3d060bb59d3bcad38dd62549641074a8c7c3
candog85 <candog851@gmail.com>
2024-12-18 10:36:44 -0500

    Competed Search Function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash
 import pymysql
 from dynaconf import Dynaconf
+import flask_login
 
 app = Flask(__name__)
 
@@ -9,6 +10,38 @@ conf = Dynaconf(
 )
 
 app.secret_key = conf.secret_key
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, user_id, username, email, full_name):
+        self.id = user_id
+        self.username = username
+        self.email = email
+        self.full_name = full_name
+
+    def get_id(self):
+        return str(self.id)
+
+@login_manager.user_loader
+def load_user(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `ID` = {user_id}")
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if result is not None:
+        return User(result['ID'], result['username'], result['email'], result['full_name'])
+
 
 
 def connect_db():
@@ -32,11 +65,16 @@ def index():
 @app.route("/browse")
 def browse():
 
+    query=request.args.get('query')
+
     conn = connect_db()
 
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM `Product`;")
+    if query is None:
+        cursor.execute("SELECT * FROM `Product`;")
+    else:
+        cursor.execute(f"SELECT * FROM `Product` WHERE `product_name` LIKE '%{query}%';")
 
     results = cursor.fetchall()
 
@@ -104,3 +142,35 @@ def signup_page():
             conn.close()
 
     return render_template("signup.html.jinja")
+
+@app.route("/signin", methods=["POST", "GET"])
+def signin_page():
+
+    if request.method=='POST':
+        username = request.form['username'].strip()
+        password = request.form['password']
+
+        conn=connect_db()
+        cursor=conn.cursor()
+
+        cursor.execute(f"SELECT * FROM `Customer` WHERE `username` = '{username}'")
+        result=cursor.fetchone()
+
+        if (result is None): 
+            flash("Incorrect Username or Password")
+        elif (result['password']!=password):
+            flash("Incorrect Username or Password")
+        else:
+            user = User(result['ID'], result['username'], result['email'], result['full_name'])
+            flask_login.login_user(user)
+            return redirect('/')
+
+
+
+
+    return render_template("signin.html.jinja")
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect('/')
